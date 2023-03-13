5eacc81b2c3969ad257e90fa83805fc14068da34
yuyasat <takiyour@gmail.com>
2023-03-13 22:50:04 +0900

    introduce hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
 import { useSelector } from 'react-redux';
 import Login from './components/login/Login';
+import { useAppSelector } from './app/hooks';
 
 function App() {
-  // const user = useSelector((state) => state.user.user)
-  const user = null;
+  const user = useAppSelector((state) => state.user)
+  console.log(user);
+  // const user = null;
   return (
     <div className="App">
       {user ? (
---
+++ src/app/hooks.ts
@@ -0,0 +1,6 @@
+import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "./store";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
